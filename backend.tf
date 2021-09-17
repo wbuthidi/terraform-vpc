a12d2b03ef1fdda2764a7dd9770763cbf5a07ccb
wbuthidi <84468316+wbuthidi@users.noreply.github.com>
2021-09-16 21:18:00 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "jenkins-terraform-evolvecybertraining"
+        bucket = "s3-class-with-evolve-cyber-willie"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
